98dc57c96e601387791d4f39381b4d0785c4762e
Maciej Kobza <maciejkobza@Maciejs-MacBook-Pro.local>
2017-12-27 00:22:49 +0100

    add own redux store

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,37 @@
 import { renderTodos } from './utils';
+import * as fromStore from './store';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
 
-button.addEventListener(
-  'click',
-  () => {
+const reducers = {
+  todos: fromStore.reducer
+};
+
+const store = new fromStore.Store(reducers);
+
+button.addEventListener('click', () => {
     if (!input.value.trim()) return;
 
-    const payload = { label: input.value, complete: false };
-
-    console.log(payload);
+    const todos = { label: input.value, complete: false };
+    
+    store.dispatch(new fromStore.AddTodo(todos));
 
     input.value = '';
   },
   false
 );
 
+const unsubscribe = store.subscribe(state => renderTodos(state.todos.data));
+destroy.addEventListener('click', unsubscribe, false);
+
 todoList.addEventListener('click', function(event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === 'button') {
-    console.log(target);
+    const todo = JSON.parse(target.getAttribute('data-todo') as any);
+
+    store.dispatch(new fromStore.RemoveTodo(todo));
   }
 });
